a8c4a5d0d4fe975b32c906f24828b93b95d311c6
JeremyKamLongbow <jeremy.kam@longbow.com.my>
2024-06-17 17:10:10 +0800

    14/6/2024

---
+++ LargeBoooks/LargeBooksWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using LargeBooksWebApp.Data;
+using LargeBooksWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LargeBooksWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the same");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+
+            
+        }
+    }
+}
---
+++ LargeBoooks/LargeBooksWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LargeBooksWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LargeBooksWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
